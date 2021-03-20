6115696cf2b066e1dd42afaaa92e7d523fd28b01
liaozhai <laocan@yandex.ru>
2021-03-20 18:48:09 +0300

    Updated

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+export default class Evented implements EventTarget {
+    private listeners = {};
+    addEventListener(type: string, callback: EventListener): void {
+        if(!(type in this.listeners)) {
+            this.listeners[type] = [];
+        }
+        this.listeners[type].push(callback);
+    }
+    on(type: string, callback: EventListener): Evented {
+        this.addEventListener(type, callback);
+        return this;
+    }
+    removeEventListener (type: string, callback: EventListener): undefined {
+        if(!(type in this.listeners)) {
+            return;
+        }
+        let stack = this.listeners[type];
+        for(let i = 0, l = stack.length; i < l; i++) {
+            if(stack[i] === callback){
+                stack.splice(i, 1);
+                return this.removeEventListener(type, callback);
+            }
+        }
+    }
+    off(type: string, callback: EventListener): Evented {
+        this.removeEventListener(type, callback);
+        return this;
+    }
+    dispatchEvent(event: Event): boolean {
+        if(!(event.type in this.listeners)) {
+            return false;
+        }
+        let stack = this.listeners[event.type];
+	    Object.defineProperty(event, 'target', {
+            enumerable: false,
+            configurable: false,
+            writable: false,
+            value: this
+        });
+        for(let i = 0, len = stack.length; i < len; i++) {
+            stack[i].call(this, event);
+        }
+        return true;
+    }
+};
